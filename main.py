210c9e41174a68ed998145cb21c019db57f52abd
SirZen476 <danielzenkin82@gmail.com>
2024-10-26 15:36:42 +0300

    done with fixes, works, had to change + and - on values bigger then -2^31 or 2^31 -1 in abs

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        st = s.strip()
+        if st == "" : return 0
         st = s.lstrip()  # remove whitespaces if present
         curr_char = st[0]
         curr_val = ord(curr_char)
@@ -16,9 +18,9 @@ class Solution(object):
         if curr_val == 45 or curr_val == 43: # 44-43 = 1 for plus , 44-45 = -1 for minus, the read next
             sign = 44-curr_val
             st = st[1:]
+            if st == "": return 0
             curr_char = st[0]
             curr_val = ord(curr_char)
-        loop_count = 0
         while(48<=curr_val<=57):
             if curr_val ==48 and leading_zero == False:
                 pass
@@ -30,7 +32,17 @@ class Solution(object):
             if len(st) == 0: break
             curr_char = st[0]
             curr_val = ord(curr_char)
-        res *= sign
+            if res*sign < -2**(31):
+                res = 2 **(31)
+                break
+            if res*sign >(2 ** 31 - 1):
+                res = 2 **31 -1
+                break
+        res = res*sign
+        if res  < -2 ** (31):
+            res = -2 ** (31)
+        if res >(2 ** 31 - 1):
+            res = 2 **31 -1
         return (res)
 
 
@@ -43,8 +55,10 @@ class Solution(object):
 
 
 sol = Solution()
-print(sol.myAtoi("  -00001217008768"))
+print(sol.myAtoi(" -2147483649"))
 #print("0:",ord("0"),"1:",ord("1"),"9:",ord("9"),)
+
+
 
 
 
