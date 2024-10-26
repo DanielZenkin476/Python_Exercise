7d3ec9763fb1994075e05613981740b12fc39cfb
SirZen476 <danielzenkin82@gmail.com>
2024-10-26 14:31:29 +0300

    problem 8 from leetcode, did previus too, need to add. this rep will house leetcode solutions

---
+++ main.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s.strip()  # remove whitespaces if present
+        print(s)
+        s = s[1:]
+        print(s)
+
+
+sol = Solution()
+sol.myAtoi(self, s)
+
+
+
+
+
+
+
