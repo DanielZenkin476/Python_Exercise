60664ced26a02c53f2ea93adc379dc238d4e8045
SirZen476 <danielzenkin82@gmail.com>
2024-10-26 15:02:09 +0300

    works for small numbers and no middle zeros, need to fix for others

--- main.py
+++ main.py
@@ -12,15 +12,36 @@ class Solution(object):
         curr_val = ord(curr_char)
         sign = 1
         leading_zero = False
-        val = 0
+        res = 0
         mult = 0
         if curr_val == 45 or curr_val == 43: # 44-43 = 1 for plus , 44-45 = -1 for minus, the read next
             sign = 44-curr_val
             st = st[1:]
             curr_char = st[0]
             curr_val = ord(curr_char)
-        print(st)
-        print(sign)
+        loop_count = 0
+        while(48<=curr_val<=57):
+            print(res)
+            if curr_val ==48 and leading_zero == False:
+                st = st[1:]
+                curr_char = st[0]
+                curr_val = ord(curr_char)
+                pass
+            else:
+                if curr_val == 48 :
+                    res = res * 10
+                else:
+                    print('curr:',curr_char)
+                    res += int(curr_char)/(10**mult)
+                    mult+=1
+                st = st[1:]
+                curr_char = st[0]
+                curr_val = ord(curr_char)
+                loop_count+=1
+        res = sign * int(res*(10**loop_count-1))
+        print(res)
+
+
 
 
 
@@ -30,8 +51,8 @@ class Solution(object):
 
 
 sol = Solution()
-sol.myAtoi("  -Test ss2   d    ")
-
+sol.myAtoi("  -123213    ")
+print("0:",ord("0"),"1:",ord("1"),"9:",ord("9"),)
 
 
 
