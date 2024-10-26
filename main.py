8f527b013a7bb47d192cbc216adb2732ff5d41e7
SirZen476 <danielzenkin82@gmail.com>
2024-10-26 15:10:17 +0300

    works, zeros work, should be fine

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ class Solution(object):
         sign = 1
         leading_zero = False
         res = 0
-        mult = 0
         if curr_val == 45 or curr_val == 43: # 44-43 = 1 for plus , 44-45 = -1 for minus, the read next
             sign = 44-curr_val
             st = st[1:]
@@ -22,17 +21,14 @@ class Solution(object):
         loop_count = 0
         while(48<=curr_val<=57):
             if curr_val ==48 and leading_zero == False:
-                st = st[1:]
-                curr_char = st[0]
-                curr_val = ord(curr_char)
                 pass
             else:
+                leading_zero = True
                 curr_int = int(curr_char)
                 res = res*10 + curr_int
-                st = st[1:]
-                curr_char = st[0]
-                curr_val = ord(curr_char)
-                loop_count+=1
+            st = st[1:]
+            curr_char = st[0]
+            curr_val = ord(curr_char)
         res *= sign
         return (res)
 
@@ -46,7 +42,7 @@ class Solution(object):
 
 
 sol = Solution()
-print(sol.myAtoi("  -12178768    "))
+print(sol.myAtoi("  -00001217008768    "))
 #print("0:",ord("0"),"1:",ord("1"),"9:",ord("9"),)
 
 
